cd5971903c79d6c4811f26a95798fd0a39ff6369
hhg000726 <ghdgusrl2000@gmail.com>
2024-04-25 13:17:27 +0900

    error correction

--- milestone2/src/main/java/com/example/MovieRepositoryService.java
+++ milestone2/src/main/java/com/example/MovieRepositoryService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class MovieRepositoryService {
@@ -12,11 +13,11 @@ public class MovieRepositoryService {
     private MovieRepository movieRepository;
 
     public void createKeywords() {
-        for (int i = 1; i <= movieRepository.count(); i++) {
-            Movie movie = movieRepository.findById(String.valueOf(i)).get();
+        for (Movie movie : movieRepository.findAll()) {
             List<String> keywords = new ArrayList<>(List.of(movie.getTitle().split(" ")));
-            keywords.retainAll(List.of(movie.getGenre().split("\\|")));
+            keywords.addAll(List.of(movie.getGenre().split("\\|")));
             movie.setKeywords(keywords);
+            movieRepository.save(movie);
         }
     }
 }
