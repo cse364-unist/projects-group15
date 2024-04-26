3cf767c449fc64d6f306226bf12631eddce65c0c
Dantalian12 <164485995+Dantalian12@users.noreply.github.com>
2024-04-26 18:22:55 +0900

    Update UserDAL.java

--- milestone2/src/main/java/com/example/UserDAL.java
+++ milestone2/src/main/java/com/example/UserDAL.java
@@ -2,4 +2,10 @@ package com.example;
 
 public interface UserDAL {
     boolean checkUserIdExists(String userId);
+
+    User getUserbyUserId(String userId);
+
+    Integer classifyAge(String age);
+
+    Integer classifyOccupation(String occupation);
 }
