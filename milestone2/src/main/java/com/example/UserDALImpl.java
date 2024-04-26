670da17341e6c6398c0d6b62504df28243dde489
Dantalian12 <164485995+Dantalian12@users.noreply.github.com>
2024-04-26 18:22:33 +0900

    Update UserDALImpl.java

--- milestone2/src/main/java/com/example/UserDALImpl.java
+++ milestone2/src/main/java/com/example/UserDALImpl.java
@@ -18,4 +18,53 @@ public class UserDALImpl implements UserDAL{
         Query query = Query.query(Criteria.where("userId").is(userId));
         return mongoTemplate.exists(query, User.class);
     }
+    
+    @Override
+    public User getUserbyUserId(String userId){
+            Query query = Query.query(Criteria.where("userId").is(userId));
+            return mongoTemplate.findOne(query, User.class);
+    }
+
+    @Override
+    public Integer classifyAge(String age){
+        return switch (age) {
+            case "1" -> 0;
+            case "18" -> 1;
+            case "25" -> 2;
+            case "35" -> 3;
+            case "45" -> 4;
+            case "50" -> 5;
+            case "56" -> 6;
+            default -> -1;
+        };
+    }
+
+    @Override
+    public Integer classifyOccupation(String occupation){
+        return switch (occupation){
+            case "0" -> 0;
+            case "1" -> 1;
+            case "2" -> 2;
+            case "3" -> 3;
+            case "4" -> 4;
+            case "5" -> 5;
+            case "6" -> 6;
+            case "7" -> 7;
+            case "8" -> 8;
+            case "9" -> 9;
+            case "10" -> 10;
+            case "11" -> 11;
+            case "12"-> 12;
+            case "13" -> 13;
+            case "14" -> 14;
+            case "15" -> 15;
+            case "16" -> 16;
+            case "17" -> 17;
+            case "18" -> 18;
+            case "19" -> 19;
+            case "20" -> 20;
+            default -> -1;
+        };
+    }
+}
 }
