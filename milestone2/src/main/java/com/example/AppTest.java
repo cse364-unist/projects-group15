eb53a15063d21c96dfab0bae96de9611585027dc
pupungeggang2 <kyongkyong2892@gmail.com>
2024-04-30 16:51:47 +0900

    Added Test

--- milestone2/src/main/java/com/example/AppTest.java
+++ milestone2/src/main/java/com/example/AppTest.java
@@ -1,5 +1,16 @@
 package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class AppTest {
-       
+    @Test
+    public void testPasswordMatchEqual() {
+        
+    }
+
+    @Test
+    public void testPasswordMatchNotEqual() {
+
+    }
 }
