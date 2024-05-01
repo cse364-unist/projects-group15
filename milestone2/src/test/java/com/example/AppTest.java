2c2842a0b8a78eb706d78abc41c18c80ed0a8c99
pupungeggang2 <kyongkyong2892@gmail.com>
2024-05-02 03:09:56 +0900

    Merge Complete

--- milestone2/src/test/java/com/example/AppTest.java
+++ milestone2/src/test/java/com/example/AppTest.java
@@ -8,8 +8,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -113,35 +112,6 @@ public class AppTest {
                 null,
                 new ParameterizedTypeReference<List<Movie>>() {}
         );
-    }
-}
-
-package com.example;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
-
-import java.util.HashMap;
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AppTest {
-    @Autowired
-    private TestRestTemplate restTemplate;
-    @LocalServerPort
-    private int port;
-    @Test
-    public void testGetUser() {
-        User user = new User("1", "F", "1", "10", "default_username", "default_password", new HashMap<>());
-        ResponseEntity<User> response = restTemplate.getForEntity("http://localhost:" + port + "/users/1", User.class);
-        System.out.println(response.getBody().getUserId());
-        assertEquals(user.getUserId(), response.getBody().getUserId());
     }
 
     // User Function
