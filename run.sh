db0814d471109b61b51429b93209ae51f3656520
pupungeggang2 <kyongkyong2892@gmail.com>
2024-06-12 10:57:42 +0900

    Adjusted DockerFile

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 # Start MongoDB server
 mongod --fork --logpath /var/log/mongodb.log
-git clone https://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@github.com/cse364-unist/projects-group15.git
-cd projects-group15 || exit
-git checkout milestone2
-cd milestone2 || exit
-mvn jacoco:report
+cd milestone3
 mvn package
 java -jar ./target/cse364-project-1.0-SNAPSHOT.jar
