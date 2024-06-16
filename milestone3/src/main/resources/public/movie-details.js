1ed6c4d2209f02de3ac566f811bcf4a050282220
hhg000726 <93714318+hhg000726@users.noreply.github.com>
2024-06-16 22:15:33 +0900

    Update movie-details.js
    design

--- milestone3/src/main/resources/public/movie-details.js
+++ milestone3/src/main/resources/public/movie-details.js
@@ -232,7 +232,7 @@ $(document).ready(function() {
             const userMovieList = JSON.parse(localStorage.getItem("userMovieList"))
             for (let listName in userMovieList) {
                 $('#selected-list-options').append(`
-                    <label><input type="checkbox" name="selected-lists" value="${listName}">${listName}</label><br>
+                    <label><input type="checkbox" name="selected-lists" value="${listName}">${listName}</label>
                 `);
             }
             $('#movie-info2').html(`
