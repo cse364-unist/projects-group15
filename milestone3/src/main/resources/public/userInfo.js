9b208283b8cacfc76e2a6cd3ec7a1666f60346b1
Dantalian12 <164485995+Dantalian12@users.noreply.github.com>
2024-06-16 17:58:23 +0900

    Add change password

--- milestone3/src/main/resources/public/userInfo.js
+++ milestone3/src/main/resources/public/userInfo.js
@@ -1,6 +1,8 @@
+
 $(document).ready(function() {
     $('#user-info-form').on('submit', function(event) {
         event.preventDefault();
+
 
         const userInfo = {
             username: $('#username').val(),
@@ -9,21 +11,47 @@ $(document).ready(function() {
             age: $('#age').val()
         };
 
+
+        const password = $('#password').val();
+        if (password) {
+            userInfo.password = password;
+        }
+
+
         const queryParams = $.param(userInfo);
+
 
         $.ajax({
             url: 'http://localhost:8080/cse364-project/changeUserInfo',
             type: 'PUT',
-            data: queryParams,
-            dataType: 'json',
+            data: queryParams, 
+            dataType: 'json', 
             success: function(response) {
                 alert('User information updated successfully!');
                 console.log(response);
             },
             error: function(error) {
-                alert('Failed to update user information. Please try again');
+                alert('Failed to update user information.');
                 console.error(error);
             }
         });
+
+
+        if (password) {
+            $.ajax({
+                url: 'http://localhost:8080/cse364-project/changeUserPassword',
+                type: 'PUT',
+                data: $.param({ username: $('#username').val(), password: password }),
+                dataType: 'json', 
+                success: function(response) {
+                    alert('Password updated successfully!');
+                    console.log(response);
+                },
+                error: function(error) {
+                    alert('Failed to update password.');
+                    console.error(error);
+                }
+            });
+        }
     });
 });
